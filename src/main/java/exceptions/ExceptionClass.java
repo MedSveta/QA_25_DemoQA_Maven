02c4768611557e33ee9e74bd8c66144753868376
MedSveta <sveta1978medved@gmail.com>
2024-04-16 21:40:36 +0300

    created exception

--- src/main/java/exceptions/ExceptionClass.java
+++ src/main/java/exceptions/ExceptionClass.java
@@ -1,2 +1,46 @@
-package exceptions;public class ExceptionClass {
+package exceptions;
+
+public class ExceptionClass {
+
+    public static void main(String[] args) {
+        //checkedExceptionSecond();
+        uncheckedExceptionIndexOfBoundsExc(1);
+        System.out.println("after exception");
+        methodWithMyException(null);
+        System.out.println("after  my exception");
+    }
+
+    public static void  methodWithMyException(String str){
+        if (str == null) try {
+            throw new MyException("it is my exception");
+        } catch (MyException e) {
+            e.printStackTrace();
+        }
+
+        else System.out.println(str);
+    }
+    public static void uncheckedExceptionIndexOfBoundsExc(int i) {
+        String[] array = {"one", "two", "three"};
+        String str = null;
+        try {
+            str = array[i];
+        } catch (ArrayIndexOutOfBoundsException e) {
+            e.printStackTrace();
+            str = array[array.length -1];
+        }
+        System.out.println("str -->"+ str);
+    }
+
+    public static void checkedExceptionSecond() {
+        try {
+            checkedException();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+
+        }
+    }
+
+    public static void checkedException() throws InterruptedException {
+        Thread.sleep(3000);
+    }
 }
--- src/main/java/exceptions/MyException.java
+++ src/main/java/exceptions/MyException.java
@@ -1,2 +1,7 @@
-package exceptions;public class MyException {
+package exceptions;
+
+public class MyException extends Exception{
+    public MyException(String message) {
+        super(message);
+    }
 }
